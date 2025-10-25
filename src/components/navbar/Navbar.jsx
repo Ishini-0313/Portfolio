45748fa47d9485213e006b514f8955eaeb0d234b
Ishini <misnfernando1@gmail.com>
2025-10-25 22:15:40 +0530

    ..

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React,{useState} from 'react';
+import {getImageUrl} from '../../utils';
+import styles from './Navbar.module.css';
 
-export default function Navbar() {
+export default function Navbar () {
+    const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <div>Navbar</div>
+    <nav className={styles.navbar}>
+        <a href="/" className={styles.title}>Portfolio</a>
+        <div className={styles.menu}>
+            <img className={styles.menuBtn} src={menuOpen?'assets/menuIcon.png':'assets/closeIcon.png'} alt='menu-btn' onClick={()=>setMenuOpen(!menuOpen)}></img>
+            <ul className={styles.menuItems}>
+                <li>
+                    <a href="#about">About</a>
+                </li>
+                <li>
+                    <a href="#experience">Experience</a>
+                </li>
+                <li>
+                    <a href="#Projects">Projects</a>
+                </li>
+                <li>
+                    <a href="#Contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
   )
 }
